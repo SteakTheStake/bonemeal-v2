a3e19ea4a452e5904ce25c5a518133e98f1b1523
Gabe Corr <gabecorr25@gmail.com>
2024-03-30 15:04:01 -0700

    0.0.1-s

--- bonemeal/src/ui/script.js
+++ bonemeal/src/ui/script.js
@@ -1,10 +1,16 @@
-import { appWindow } from '@tauri-apps/api/window'
-document
-    .getElementById('titlebar-minimize')
-    .addEventListener('click', () => appWindow.minimize())
-document
-    .getElementById('titlebar-maximize')
-    .addEventListener('click', () => appWindow.toggleMaximize())
-document
-    .getElementById('titlebar-close')
-    .addEventListener('click', () => appWindow.close())
+const appWindow = window.window.getCurrent();
+
+// Maximize the window
+document.getElementById('titlebar-maximize').addEventListener('click', () => {
+    appWindow.maximize();
+});
+
+// Minimize the window
+document.getElementById('titlebar-minimize').addEventListener('click', () => {
+    appWindow.minimize();
+});
+
+// Close the window
+document.getElementById('titlebar-close').addEventListener('click', () => {
+    appWindow.close();
+});
